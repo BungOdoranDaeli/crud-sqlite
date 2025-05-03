42dd66404afe3ef1201adc382dd6e822c4ce3a7b
BungOdoranDaeli <bungodorandaeli04072006@gmail.com>
2025-05-03 20:12:34 +0700

    menambahkan file index.php dengan fungsi CRUD ke sqlite

---
+++ 000-tugas.sql
@@ -0,0 +1,19 @@
+-- 000-tugas.sql
+
+-- Membuat tabel tugas
+CREATE TABLE "tugas" (
+    "id" INTEGER,
+    "deskripsi" TEXT NOT NULL,
+    "waktu" INTEGER NOT NULL,
+    PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+-- Menambahkan data ke tabel tugas
+INSERT INTO "tugas" ("deskripsi", "waktu") VALUES
+('makan pagi', 60),
+('kuliah', 360),
+('latihan bulu tangkis', 60),
+('olah raga', 150),
+('jalan-jalan', 100),
+('berenang', 60),
+('tidur', 480);
